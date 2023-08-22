49e26ba6f456c447581638cf615976c5237a443e
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-08-22 14:04:10 +0800

    假如线程模式

--- NoaEngine/Scene.cpp
+++ NoaEngine/Scene.cpp
@@ -1,1 +1,48 @@
 #include "Scene.h"
+
+namespace noa 
+{
+	MapFile LoadMap(const char* fileName)
+	{
+		MapFile map;
+
+		std::ifstream file(fileName, std::ios::binary);
+		if (!file)
+		{
+			std::cout << "无法打开文件" << std::endl;
+			//map.image = nullptr;
+			return map;
+		}
+
+		file.read(reinterpret_cast<char*>(&map.w), sizeof(int));
+		file.read(reinterpret_cast<char*>(&map.h), sizeof(int));
+
+		int imageDataSize = map.w * map.h * sizeof(uint8_t);
+		//map.image = new uint8_t[imageDataSize];
+		map.image.resize(imageDataSize);
+		file.read(reinterpret_cast<char*>(map.image.data()), imageDataSize);
+
+		file.close();
+
+		return map;
+	}
+
+	LevelMap::LevelMap() {
+
+	}
+
+	LevelMap::LevelMap(MapFile map)
+	{
+		this->w = map.w;
+		this->h = map.h;
+
+		level = map.image;
+
+	}
+
+	TileMap::TileMap(unordered_map<Uint8, Tile> tileSet, MapFile map) :LevelMap(map)
+	{
+		this->tileSet = tileSet;
+	}
+
+}
--- NoaEngine/Scene.h
+++ NoaEngine/Scene.h
@@ -13,6 +13,7 @@
 #include <string>
 
 #include "NoaMath.h"
+#include "Sprite.h"
 
 using namespace std;
 
@@ -29,105 +30,38 @@ namespace noa {
 
 
 	//加载地图
-	inline MapFile LoadMap(const char* fileName)
-	{
-		MapFile map;
-
-		std::ifstream file(fileName, std::ios::binary);
-		if (!file)
-		{
-			std::cout << "无法打开文件" << std::endl;
-			//map.image = nullptr;
-			return map;
-		}
-
-		file.read(reinterpret_cast<char*>(&map.w), sizeof(int));
-		file.read(reinterpret_cast<char*>(&map.h), sizeof(int));
-
-		int imageDataSize = map.w * map.h * sizeof(uint8_t);
-		//map.image = new uint8_t[imageDataSize];
-		map.image.resize(imageDataSize);
-		file.read(reinterpret_cast<char*>(map.image.data()), imageDataSize);
-
-		file.close();
-
-		return map;
-	}
+	extern MapFile LoadMap(const char* fileName);
 
 	class LevelMap {
-	private:
-
-
 	public:
 		vector<uint8_t> level;
-		//Uint8* level = nullptr;
 		int w = 0;
 		int h = 0;
 	public:
-		LevelMap() {
-
-		}
-
-		LevelMap(MapFile map)
-		{
-			this->w = map.w;
-			this->h = map.h;
-
-			level = map.image;
-
-		}
-
-		LevelMap(int width, int height, wstring map)
-		{
-			this->w = width;
-			this->h = height;
-			//level = new Uint8(width * height);
-
-			//加载地图到缓存中
-			level.clear();
-			for (int i = 0; i < map.length(); i++)
-			{
-				level.push_back(map[i]);
-				//level[i] = map[i];
-			}
-
-		}
-
-		void LoadMap(wstring map)
-		{
-			for (int i = 0; i < map.length(); i++)
-			{
-				level[i] = map[i];
-			}
-		}
-
-		std::wstring LoadLevel(const char* filePath) {
-			std::wstring content;
-
-			std::ifstream file(filePath, std::ios::binary);
-
-			if (file.is_open()) {
-				file.seekg(0, std::ios::end);
-				size_t length = file.tellg();
-				file.seekg(0, std::ios::beg);
-
-				char* buffer = new char[length];
-				file.read(buffer, length);
-
-				content = std::wstring(reinterpret_cast<const wchar_t*>(buffer), length / sizeof(wchar_t));
-
-				delete[] buffer;
-
-				file.close();
-			}
-			else {
-				std::cerr << "无法打开文件。" << std::endl;
-			}
-
-			return content;
-		}
-
+		LevelMap();
+		LevelMap(MapFile map);
 	};
+
+	//瓦片地图
+	typedef struct Tile {
+		Sprite sprite;
+		bool isCollision = false;
+		Tile(Sprite sprite,bool isCollision = false) 
+		{
+			this->sprite = sprite;
+			this->isCollision = isCollision;
+		}
+	}Tile;
+
+	//瓦片地图
+	class TileMap:public LevelMap
+	{
+	private:
+		unordered_map<Uint8, Tile> tileSet;
+	public:
+		TileMap(unordered_map<Uint8,Tile> tileSet,MapFile map);
+	};
+
 }
 
 
